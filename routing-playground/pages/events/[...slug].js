9fd8608fe69fd0aab53e9768597eecee3940ece6
Praveen <tkmpraveens@gmail.com>
2023-03-03 17:50:34 +0530

    initial set up

---
+++ routing-playground/pages/events/[...slug].js
@@ -0,0 +1,5 @@
+const FilteredEventList = () => {
+  return <div>Filtered event list</div>;
+};
+
+export default FilteredEventList;
---
+++ routing-playground/pages/events/index.js
@@ -0,0 +1,5 @@
+const EventList = () => {
+  return <div>Event list</div>;
+};
+
+export default EventList;
